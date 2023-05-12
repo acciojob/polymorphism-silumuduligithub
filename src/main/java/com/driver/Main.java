757e688098c20c05e3b4b1c54336fa3f3430497f
silulitgithub <kumarsilu530@gmail.com>
2023-05-13 00:52:42 +0530

    second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,29 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("ok");
-        System.out.println("hello world");
-    }
+    public class Main {
+        public static class Product {
+            public int product(int x, int y) {
+                return x*y;
+            }
+
+            public int product(int x, int y, int z) {
+                return x*y*z;
+            }
+
+            public double product(double x, double y) {
+                return x*y;
+            }
+        }
+        public static void main(String[] args) {
+            Product p=new Product();
+
+            System.out.println(p.product(1,2));
+
+            System.out.println(p.product(1,2,3));
+
+            double x=1.0,y=2.0;
+
+            System.out.println(p.product(x,y));
+        }
 }
